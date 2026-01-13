ea40b0d0719d94d607e68858a43eb84dbfbfe77b
amitmore07 <amitmore50921@gmail.com>
2026-01-13 13:01:59 +0530

    margintop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsDashboardComponent } from './components/products-dashboard/products-dashboard.component';
 import { PostsDashboardComponent } from './components/posts-dashboard/posts-dashboard.component';
 import { UsersDashboardComponent } from './components/users-dashboard/users-dashboard.component';
-import { HomeComponent } from './components/home/home.component';
+
 
 const routes: Routes = [
 
-
-  {
-    path:'home',
-    component:HomeComponent
-  },
   {
     path:'',
     redirectTo:"home",
